101fa7b7e85391974189b719e6eb46052cf63ff6
Don Carlier <doncarlier@gmail.com>
2020-11-28 10:06:58 +0100

    init commit

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the Todo List app v1.0.0. It is part of the Vue crash course on YouTube.</p>
+  </div>
+</template>
+
+<style scoped>
+    .about {
+      padding: 16px;  
+    }
+</style>
